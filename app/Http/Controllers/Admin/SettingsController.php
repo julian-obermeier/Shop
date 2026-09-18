6bf5a6ebeab0c5285c7025f3b7054658f86ba645
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 15:33:06 +0200

    Add private file retention settings

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -16,6 +16,10 @@ class SettingsController extends Controller
             'proof_reminders_enabled'=>Setting::valueOf('proof_reminders_enabled',true),
             'email_notifications_enabled'=>Setting::valueOf('email_notifications_enabled',true),
             'support_email'=>Setting::valueOf('support_email',''),
+            'identity_retention_days'=>Setting::valueOf('identity_retention_days',30),
+            'precheck_retention_days'=>Setting::valueOf('precheck_retention_days',180),
+            'proof_retention_days'=>Setting::valueOf('proof_retention_days',365),
+            'message_attachment_retention_days'=>Setting::valueOf('message_attachment_retention_days',365),
         ];
         return view('admin.settings.index',compact('settings'));
     }
@@ -26,6 +30,10 @@ class SettingsController extends Controller
             'site_name'=>['required','string','max:120'],
             'minimum_payout'=>['required','numeric','min:0'],
             'support_email'=>['nullable','email','max:255'],
+            'identity_retention_days'=>['required','integer','min:1','max:3650'],
+            'precheck_retention_days'=>['required','integer','min:1','max:3650'],
+            'proof_retention_days'=>['required','integer','min:1','max:3650'],
+            'message_attachment_retention_days'=>['required','integer','min:1','max:3650'],
         ]);
 
         $pairs=[
@@ -34,6 +42,10 @@ class SettingsController extends Controller
             'proof_reminders_enabled'=>[$request->boolean('proof_reminders_enabled')?'1':'0','bool'],
             'email_notifications_enabled'=>[$request->boolean('email_notifications_enabled')?'1':'0','bool'],
             'support_email'=>[$data['support_email']??'','string'],
+            'identity_retention_days'=>[(string)$data['identity_retention_days'],'int'],
+            'precheck_retention_days'=>[(string)$data['precheck_retention_days'],'int'],
+            'proof_retention_days'=>[(string)$data['proof_retention_days'],'int'],
+            'message_attachment_retention_days'=>[(string)$data['message_attachment_retention_days'],'int'],
         ];
 
         foreach($pairs as $key=>[$value,$type]){
