7e9f89d841ab5aef2972991f3c1ca30bcea4fd7d
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 14:50:02 +0200

    Add UserNotification

---
+++ app/Models/UserNotification.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+class UserNotification extends Model {
+    protected $guarded=[];
+    protected function casts(): array { return ['data'=>'array','read_at'=>'datetime']; }
+    public function user(): BelongsTo { return $this->belongsTo(User::class); }
+}
