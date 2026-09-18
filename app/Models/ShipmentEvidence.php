3e7a6acec2202826816e6c8fc90dd3b41a4dbe37
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 19:18:03 +0200

    feat: add shipment evidence model

---
+++ app/Models/ShipmentEvidence.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ShipmentEvidence extends Model
+{
+    protected $guarded=[];
+
+    public function shipment(): BelongsTo { return $this->belongsTo(Shipment::class); }
+    public function user(): BelongsTo { return $this->belongsTo(User::class); }
+}
