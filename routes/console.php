30abcb97cec687896ec81de136adea6b797870c3
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 19:04:00 +0200

    feat: schedule payout auto completion

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('proofs:remind')->hourly();
 Schedule::command('orders:deadlines')->everyFifteenMinutes();
+Schedule::command('payouts:complete-executed')->hourly();
 
 Schedule::command('privacy:purge')->dailyAt('03:30');
