4000d623025131055775fc841c589bd4135ee7d2
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 15:21:32 +0200

    Enable built-in email verification for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,22 +1,29 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable;
+    use Notifiable, MustVerifyEmailTrait;
 
     protected $fillable=['role','first_name','last_name','birth_date','email','password','status','verified_at'];
     protected $hidden=['password','remember_token'];
 
     protected function casts(): array
     {
-        return ['birth_date'=>'date','email_verified_at'=>'datetime','verified_at'=>'datetime','password'=>'hashed'];
+        return [
+            'birth_date'=>'date',
+            'email_verified_at'=>'datetime',
+            'verified_at'=>'datetime',
+            'password'=>'hashed',
+        ];
     }
 
     public function profile(): HasOne { return $this->hasOne(UserProfile::class); }
