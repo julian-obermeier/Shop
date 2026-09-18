3cbf5963b60907bc659a9f91d0d9d4bb30e9fc57
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 13:57:37 +0200

    Add Message model

---
+++ app/Models/Message.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+class Message extends Model {
+    protected $guarded=[];
+    protected function casts(): array { return ['read_at'=>'datetime']; }
+    public function conversation(): BelongsTo { return $this->belongsTo(Conversation::class); }
+    public function user(): BelongsTo { return $this->belongsTo(User::class); }
+}
