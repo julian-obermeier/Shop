7c85ebc867708c6d0777d79a2b1cf955d39db945
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 19:02:35 +0200

    feat: secure stored payout destinations

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -1,4 +1,21 @@
 <?php
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
-class UserProfile extends Model { protected $guarded = []; }
+
+class UserProfile extends Model
+{
+    protected $guarded=[];
+
+    protected function casts(): array
+    {
+        return [
+            'bank_iban'=>'encrypted',
+            'bank_account_holder'=>'encrypted',
+            'paypal_email'=>'encrypted',
+            'paypal_name'=>'encrypted',
+            'payout_details_changed_at'=>'datetime',
+            'payout_name_approved_at'=>'datetime',
+        ];
+    }
+}
