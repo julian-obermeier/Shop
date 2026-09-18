feaea868100533765290094822e0f64e9bc3c831
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 19:16:59 +0200

    feat: schedule waitlist allocation

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('proofs:remind')->everyFiveMinutes();
 Schedule::command('orders:deadlines')->everyFifteenMinutes();
+Schedule::command('offers:process-waitlists')->everyFiveMinutes();
 Schedule::command('payouts:complete-executed')->hourly();
 
 Schedule::command('privacy:purge')->dailyAt('03:30');
