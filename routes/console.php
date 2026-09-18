f15fefc030682ecf4f1878be3b98ba3373c77dfd
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 15:33:19 +0200

    Schedule daily privacy file cleanup

--- routes/console.php
+++ routes/console.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('proofs:remind')->hourly();
 Schedule::command('orders:deadlines')->everyFifteenMinutes();
+
+Schedule::command('privacy:purge')->dailyAt('03:30');
