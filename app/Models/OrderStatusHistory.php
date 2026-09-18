9028a2e616927404e7610e3c5d641b72a70e47d0
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 20:28:17 +0200

    fix: map order status history to legacy singular table

--- app/Models/OrderStatusHistory.php
+++ app/Models/OrderStatusHistory.php
@@ -1,4 +1,10 @@
 <?php
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
-class OrderStatusHistory extends Model { protected $guarded=[]; }
+
+class OrderStatusHistory extends Model
+{
+    protected $table='order_status_history';
+    protected $guarded=[];
+}
