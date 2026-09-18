3ebda5ccc7bbd3aa4ad0a2b872339ce8def35c4f
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 15:01:14 +0200

    Add RBAC and admin 2FA to bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\EnsureActiveAccount;
 use App\Http\Middleware\EnsureAdmin;
+use App\Http\Middleware\EnsurePermission;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -19,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin'=>EnsureAdmin::class,
             'active'=>EnsureActiveAccount::class,
+            'permission'=>EnsurePermission::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
