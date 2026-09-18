2bd46c0450da3c0f96d418e99950febfd8eecb37
julian-obermeier <jeyohtv@gmail.com>
2026-09-18 15:04:13 +0200

    Complete consent and notification settings in resources/views/admin/settings/index.blade.php

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -8,6 +8,7 @@
 <label>Mindestauszahlung (€)<input type="number" min="0" step="0.01" name="minimum_payout" value="{{ $settings['minimum_payout'] }}" required></label>
 <label class="full">Support-E-Mail<input type="email" name="support_email" value="{{ $settings['support_email'] }}"></label>
 <label class="check full"><input type="checkbox" name="proof_reminders_enabled" value="1" @checked($settings['proof_reminders_enabled'])><span>Automatische Erinnerungen bei fehlenden Tagesnachweisen aktivieren</span></label>
+<label class="check full"><input type="checkbox" name="email_notifications_enabled" value="1" @checked($settings['email_notifications_enabled'])><span>Benachrichtigungen zusätzlich per E-Mail versenden</span></label>
 <div class="full"><button class="btn primary">Einstellungen speichern</button></div>
 </form>
 </div>
