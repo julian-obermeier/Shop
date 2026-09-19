2897074a051b3ccb5e33d0216d8077746af7c0b0
julian-obermeier <jeyohtv@gmail.com>
2026-09-19 16:01:45 +0200

    feat: make unassigned shipment records final and immutable

--- app/Models/UnassignedShipment.php
+++ app/Models/UnassignedShipment.php
@@ -16,5 +16,16 @@ class UnassignedShipment extends Model
         ];
     }
 
+    protected static function booted(): void
+    {
+        static::updating(function(){
+            throw new \LogicException('Endgültig dokumentierte nicht zuordenbare Sendungen sind unveränderlich.');
+        });
+
+        static::deleting(function(){
+            throw new \LogicException('Nicht zuordenbare Sendungen dürfen nach der endgültigen Dokumentation nicht gelöscht werden.');
+        });
+    }
+
     public function recorder(): BelongsTo { return $this->belongsTo(User::class,'recorded_by'); }
 }
