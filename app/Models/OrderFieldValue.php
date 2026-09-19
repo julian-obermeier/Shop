4a72cdcdaff2c45423f165f6d1988de52e490ed6
julian-obermeier <jeyohtv@gmail.com>
2026-09-19 16:11:23 +0200

    feat: protect order field snapshot with controlled anonymization

--- app/Models/OrderFieldValue.php
+++ app/Models/OrderFieldValue.php
@@ -13,6 +13,22 @@ class OrderFieldValue extends Model
         return ['field_snapshot'=>'array'];
     }
 
+    protected static function booted(): void
+    {
+        static::updating(function(){
+            throw new \LogicException('Auftragsspezifische Auswahl- und Eingabewerte sind nach der Anfrage unveränderlich.');
+        });
+
+        static::deleting(function(){
+            throw new \LogicException('Auftragsspezifische Auswahl- und Eingabewerte dürfen nicht gelöscht werden.');
+        });
+    }
+
+    public function anonymizeForDeletedUser(): void
+    {
+        $this->forceFill(['value'=>null])->saveQuietly();
+    }
+
     public function order(): BelongsTo
     {
         return $this->belongsTo(Order::class);
