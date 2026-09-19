2f3ca0f0f4e0511887a69fbb3e8f8074c938e842
julian-obermeier <jeyohtv@gmail.com>
2026-09-19 16:11:19 +0200

    feat: make selected order option snapshots immutable

--- app/Models/OrderOption.php
+++ app/Models/OrderOption.php
@@ -1,4 +1,25 @@
 <?php
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
-class OrderOption extends Model { protected $guarded=[]; protected function casts(): array { return ['price_delta'=>'decimal:2','snapshot'=>'array']; } }
+
+class OrderOption extends Model
+{
+    protected $guarded=[];
+
+    protected function casts(): array
+    {
+        return ['price_delta'=>'decimal:2','snapshot'=>'array'];
+    }
+
+    protected static function booted(): void
+    {
+        static::updating(function(){
+            throw new \LogicException('Gewählte Auftragsoptionen sind Bestandteil des unveränderlichen Auftragssnapshots.');
+        });
+
+        static::deleting(function(){
+            throw new \LogicException('Gewählte Auftragsoptionen dürfen nicht aus einem bestehenden Auftrag gelöscht werden.');
+        });
+    }
+}
