e3aa2b6fb6f9616b06e7a950bd016a3d00b4e65b
julian-obermeier <jeyohtv@gmail.com>
2026-09-19 09:38:00 +0200

    feat: make wallet ledger append only

--- app/Models/WalletLedgerEntry.php
+++ app/Models/WalletLedgerEntry.php
@@ -1,4 +1,25 @@
 <?php
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
-class WalletLedgerEntry extends Model { protected $guarded=[]; protected function casts(): array { return ['amount'=>'decimal:2','metadata'=>'array']; } }
+
+class WalletLedgerEntry extends Model
+{
+    protected $guarded=[];
+
+    protected function casts(): array
+    {
+        return ['amount'=>'decimal:2','metadata'=>'array'];
+    }
+
+    protected static function booted(): void
+    {
+        static::updating(function(){
+            throw new \LogicException('Wallet-Ledger-Einträge sind unveränderlich.');
+        });
+
+        static::deleting(function(){
+            throw new \LogicException('Wallet-Ledger-Einträge dürfen nicht gelöscht werden.');
+        });
+    }
+}
